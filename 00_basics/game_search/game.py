ebaef9e1f409f96e9c58977cd8c544d470570e1b
mbjemaa <munirbenjemaa@gmail.com>
2026-08-09 16:33:31 -0400

    minimax

---
+++ 00_basics/game_search/game.py
@@ -0,0 +1,21 @@
+from typing import Protocol, TypeVar
+
+State = TypeVar("State")
+Move = TypeVar("Move")
+Player = TypeVar("Player")
+
+class Game(Protocol[State, Move, Player]):
+    def legal_moves(self, state: State) -> list[Move]:
+        ...
+    
+    def make_move(self, state: State, move: Move) -> State:
+        ...
+
+    def is_terminal(self, state: State) -> bool:
+        ...
+    
+    def evaluate(self, state: State, player: Player) -> float:
+        ...
+
+    def current_player(self, state: State) -> Player:
+        ...
